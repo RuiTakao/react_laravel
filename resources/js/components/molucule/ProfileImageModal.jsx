c32a9f2be23ab43baa1f9fa8600839dd5bc99de6
RuiTakao <ruia1082halfnc@gmail.com>
2024-04-06 14:55:25 +0900

    プロフィール編集画面切り分け

--- resources/js/components/molucule/ProfileImageModal.jsx
+++ resources/js/components/molucule/ProfileImageModal.jsx
@@ -6,27 +6,93 @@ import {
     ModalContent,
     Flex,
 } from "@chakra-ui/react";
+import { useCallback, useState } from "react";
 import Cropper from "react-easy-crop";
 
 export const ProfileImageModal = props => {
-    const {isOpen, onClose, image, crop, zoom, aspect, onCropChange, onZoomChange, onCropComplete, onClickImage} = props
+
+    const { isOpenProfileImageEdit, onCloseProfileImageEdit, base64Images, setViewImage, setCanvasData } = props
+
+    const [crop, onCropChange] = useState({ x: 0, y: 0 });
+    const [zoom, onZoomChange] = useState(1);
+    // 切り抜き箇所
+    const [croppedArea, setCroppedArea] = useState("");
+    const onCropComplete = (croppedArea, croppedAreaPixels) => setCroppedArea(croppedAreaPixels);
+
+    // 画像切り抜き
+    const onClickImage = useCallback(async () => {
+        const croppedImage = await getCroppedImg(base64Images, croppedArea)
+        setViewImage(croppedImage)
+        onCloseProfileImageEdit()
+    })
+
+    const createImage = url =>
+        new Promise((resolve, reject) => {
+            const image = new Image()
+            image.addEventListener("load", () => resolve(image));
+            image.addEventListener("error", (error) => reject(error));
+            image.setAttribute("crossOrigin", "anonymous");
+            image.src = url;
+        })
+
+    async function getCroppedImg(imageSrc, pixelCrop) {
+        const image = await createImage(imageSrc);
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+
+        if (!ctx) {
+            return "";
+        }
+
+        // canvasサイズを設定
+        canvas.width = image.width;
+        canvas.height = image.height;
+
+        // canvas上に画像を描画
+        ctx.drawImage(image, 0, 0);
+
+        // トリミング後の画像を抽出
+        const data = ctx.getImageData(
+            pixelCrop.x,
+            pixelCrop.y,
+            pixelCrop.width,
+            pixelCrop.height
+        );
+
+        // canvasのサイズ指定(切り取り後の画像サイズに更新)
+        canvas.width = pixelCrop.width;
+        canvas.height = pixelCrop.height;
+
+        // 抽出した画像データをcanvasの左隅に貼り付け
+        ctx.putImageData(data, 0, 0);
+
+        setCanvasData(canvas)
+
+        // canvasを画像に変換
+        return new Promise((resolve, reject) => {
+            canvas.toBlob((file) => {
+                if (file !== null) resolve(URL.createObjectURL(file));
+            }, "image/jpeg");
+        });
+    }
+
     return (
-        <Modal isOpen={isOpen} onClose={onClose} size="4xl">
+        <Modal isOpen={isOpenProfileImageEdit} onClose={onCloseProfileImageEdit} size="4xl">
             <ModalOverlay />
             <ModalContent p={8}>
                 <Box mt={4} w="100%" h="500px" position={"relative"}>
                     <Cropper
-                        image={image}
+                        image={base64Images}
                         crop={crop}
                         zoom={zoom}
-                        aspect={aspect}
+                        aspect={1}
                         onCropChange={onCropChange}
                         onZoomChange={onZoomChange}
                         onCropComplete={onCropComplete}
                     />
                 </Box>
                 <Flex mt={4} justify={"space-between"}>
-                    <Button onClick={onClose}>キャンセル</Button>
+                    <Button onClick={onCloseProfileImageEdit}>キャンセル</Button>
                     <Button onClick={onClickImage} bg="teal.400" color="white">切り取り</Button>
                 </Flex>
             </ModalContent>
--- resources/js/components/molucule/ProfileModal.jsx
+++ resources/js/components/molucule/ProfileModal.jsx
@@ -15,8 +15,6 @@ import {
 import { TbCameraPlus } from "react-icons/tb";
 import { ProfileImageModal } from "./ProfileImageModal"
 import { memo, useCallback, useEffect, useState } from "react";
-import Cropper from "react-easy-crop";
-import { getProfile } from "../hooks/ProfileData";
 import axios from "axios";
 
 export const ProfileModal = memo(props => {
@@ -24,7 +22,6 @@ export const ProfileModal = memo(props => {
     const { isOpen, onClose, data } = props;
 
     if (isOpen) {
-
 
         // プロフィール画像編集モーダル動作
         const {
@@ -44,79 +41,14 @@ export const ProfileModal = memo(props => {
             onOpenProfileImageEdit()
         }
 
-        // 画像切り抜き
-        const onClickImage = useCallback(async () => {
-            const croppedImage = await getCroppedImg(base64Images, croppedArea)
-            setViewImage(croppedImage)
-            onCloseProfileImageEdit()
-        })
-
-        const createImage = url =>
-            new Promise((resolve, reject) => {
-                const image = new Image()
-                image.addEventListener("load", () => resolve(image));
-                image.addEventListener("error", (error) => reject(error));
-                image.setAttribute("crossOrigin", "anonymous");
-                image.src = url;
-            })
-
-        async function getCroppedImg(imageSrc, pixelCrop) {
-            const image = await createImage(imageSrc);
-            const canvas = document.createElement("canvas");
-            const ctx = canvas.getContext("2d");
-
-            if (!ctx) {
-                return "";
-            }
-
-            // canvasサイズを設定
-            canvas.width = image.width;
-            canvas.height = image.height;
-
-            // canvas上に画像を描画
-            ctx.drawImage(image, 0, 0);
-
-            // トリミング後の画像を抽出
-            const data = ctx.getImageData(
-                pixelCrop.x,
-                pixelCrop.y,
-                pixelCrop.width,
-                pixelCrop.height
-            );
-
-            // canvasのサイズ指定(切り取り後の画像サイズに更新)
-            canvas.width = pixelCrop.width;
-            canvas.height = pixelCrop.height;
-
-            // 抽出した画像データをcanvasの左隅に貼り付け
-            ctx.putImageData(data, 0, 0);
-
-            setCanvasData(canvas)
-
-            // canvasを画像に変換
-            return new Promise((resolve, reject) => {
-                canvas.toBlob((file) => {
-                    if (file !== null) resolve(URL.createObjectURL(file));
-                }, "image/jpeg");
-            });
-        }
-
         // 画像クロップ
         // アップロードした画像
         const [base64Images, setBase64Images] = useState("")
         // 表示用
         const [viewImage, setViewImage] = useState(`/dev/react_laravel/public/storage/img/image.png`)
-        // 切り抜き箇所
-        const [croppedArea, setCroppedArea] = useState("")
         // サーバーに送る画像の情報
         const [canvasData, setCanvasData] = useState("")
-
-        const [crop, onCropChange] = useState({ x: 0, y: 0 })
-        const [zoom, onZoomChange] = useState(1)
-        const onCropComplete = (croppedArea, croppedAreaPixels) => setCroppedArea(croppedAreaPixels);
-
-
-
+        
         // テキスト入力項目取得処理
         const [name, setName] = useState(data.name);
         const [work, setWork] = useState(data.work);
@@ -156,26 +88,7 @@ export const ProfileModal = memo(props => {
                         </Flex>
                     </ModalContent>
                 </Modal>
-                <Modal isOpen={isOpenProfileImageEdit} onClose={onCloseProfileImageEdit} size="4xl">
-                    <ModalOverlay />
-                    <ModalContent p={8}>
-                        <Box mt={4} w="100%" h="500px" position={"relative"}>
-                            <Cropper
-                                image={base64Images}
-                                crop={crop}
-                                zoom={zoom}
-                                aspect={1}
-                                onCropChange={onCropChange}
-                                onZoomChange={onZoomChange}
-                                onCropComplete={onCropComplete}
-                            />
-                        </Box>
-                        <Flex mt={4} justify={"space-between"}>
-                            <Button onClick={onCloseProfileImageEdit}>キャンセル</Button>
-                            <Button onClick={onClickImage} bg="teal.400" color="white">切り取り</Button>
-                        </Flex>
-                    </ModalContent>
-                </Modal>
+                <ProfileImageModal isOpenProfileImageEdit={isOpenProfileImageEdit} onCloseProfileImageEdit={onCloseProfileImageEdit} base64Images={base64Images} setViewImage={setViewImage} setCanvasData={setCanvasData} />
             </>
         )
     }
